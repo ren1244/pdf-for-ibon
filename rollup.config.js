963d75f61fec94496a297ce908c243aef78fea4e
ren1244 <n1244506804@hotmail.com>
2022-11-07 21:12:46 +0800

    make bundle

--- rollup.config.js
+++ rollup.config.js
@@ -3,18 +3,35 @@ import commonjs from '@rollup/plugin-commonjs';
 import { terser } from "rollup-plugin-terser";
 import json from '@rollup/plugin-json';
 
-export default {
-    input: 'src/main.js',
-    output: {
-        file: 'dist/bundle.js',
-        format: 'iife',
+export default [
+    {
+        input: 'src/main.js',
+        output: {
+            file: 'dist/bundle.js',
+            format: 'iife',
+            plugins: [
+                terser()
+            ]
+        },
         plugins: [
-            terser()
+            json(),
+            nodeResolve(),
+            commonjs(),
         ]
     },
-    plugins: [
-        json(),
-        nodeResolve(),
-        commonjs(),
-    ]
-};
+    {
+        input: 'src/main-custom.js',
+        output: {
+            file: 'dist/custom-bundle.js',
+            format: 'iife',
+            plugins: [
+                terser()
+            ]
+        },
+        plugins: [
+            json(),
+            nodeResolve(),
+            commonjs(),
+        ]
+    },
+];
